b9295f89bad70d6c757f13799940c59c6cb600db
Abraham Raji <work@abrahamr.in>
2023-05-30 04:06:58 +0530

    Add treesitter

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,16 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "ruby", "javascript", "typescript", "zig" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
