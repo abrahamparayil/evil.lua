8e5d931c7260acaf80a817ec71e99ca04381ac70
Abraham Raji <work@abrahamr.in>
2023-05-30 04:19:11 +0530

    Add fugitive

--- lua/aurora/packer.lua
+++ lua/aurora/packer.lua
@@ -8,8 +8,9 @@ return require('packer').startup(function(use)
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 	use { "ellisonleao/gruvbox.nvim" }
-	use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+	use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+	use { 'tpope/vim-fugitive'}
 end)
